5332f63e386e6624a36cd639009b3b507a40722b
İsmail Yılmaz <iylmz1707@gmail.com>
2021-05-12 16:58:17 +0300

    2.Bölüm ödev

--- MvcProject/DataAccessLayer/Abstract/IRepository.cs
+++ MvcProject/DataAccessLayer/Abstract/IRepository.cs
@@ -14,7 +14,6 @@ namespace DataAccessLayer.Abstract
         void Insert(T p);
         T Get(Expression<Func<T, bool>> filter);
         void Delete(T p);
-
         void Update(T p);
         List<T> List(Expression<Func<T, bool>> filter);
 
--- MvcProject/MvcProject/Controllers/AdminCategoryController.cs
+++ MvcProject/MvcProject/Controllers/AdminCategoryController.cs
@@ -13,56 +13,56 @@ namespace MvcProject.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
-        CategoryManager cm = new CategoryManager(new EfCategoryDal());
-        public ActionResult Index()
-        {
-            var categoryValues = cm.GetList();
-            return View(categoryValues);
-        }
-        [HttpGet]
-        public ActionResult AddCategory()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult AddCategory(Category p)
-        {
-            CategoryValidator categoryValidator = new CategoryValidator();
-            ValidationResult results = categoryValidator.Validate(p);
-            if (results.IsValid)
-            {
-                cm.CategoryAdd(p);
-                return RedirectToAction("Index");
+         // GET: AdminCategory
+         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+         public ActionResult Index()
+         {
+             var categoryValues = cm.GetList();
+             return View(categoryValues);
+         }
+         [HttpGet]
+         public ActionResult AddCategory()
+         {
+             return View();
+         }
+         [HttpPost]
+         public ActionResult AddCategory(Category p)
+         {
+             CategoryValidator categoryValidator = new CategoryValidator();
+             ValidationResult results = categoryValidator.Validate(p);
+             if (results.IsValid)
+             {
+                 cm.CategoryAdd(p);
+                 return RedirectToAction("Index");
 
-            }
-            else
-            {
-                foreach (var item in results.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
-            }
-            return View();
-        }
-        public ActionResult DeleteCategory(int id)
-        {
-            var categoryvalue = cm.GetByID(id);
-            cm.CategoryDelete(categoryvalue);
-            return RedirectToAction("Index");
-        }
-        [HttpGet]
-        public ActionResult EditCategory(int id)
-        {
-            var categoryvalue = cm.GetByID(id);
-            return View(categoryvalue);
-        }
-        [HttpPost]
-        public ActionResult EditCategory(Category p)
-        {
-            cm.CategoryUpdate(p);
-            return RedirectToAction("Index");
+             }
+             else
+             {
+                 foreach (var item in results.Errors)
+                 {
+                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                 }
+             }
+             return View();
+         }
+         public ActionResult DeleteCategory(int id)
+         {
+             var categoryvalue = cm.GetByID(id);
+             cm.CategoryDelete(categoryvalue);
+             return RedirectToAction("Index");
+         }
+         [HttpGet]
+         public ActionResult EditCategory(int id)
+         {
+             var categoryvalue = cm.GetByID(id);
+             return View(categoryvalue);
+         }
+         [HttpPost]
+         public ActionResult EditCategory(Category p)
+         {
+             cm.CategoryUpdate(p);
+             return RedirectToAction("Index");
 
-        }
+         }
     }
 }
